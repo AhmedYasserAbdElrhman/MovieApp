58d9d5663de8437cee051da8ef96186fbfcb3d71
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2025-05-20 23:36:57 +0300

    Create NetworkClientProtocol.swift

---
+++ Network/Sources/Network/NetworkClient/NetworkClientProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkClientProtocol.swift
+//  Network
+//
+//  Created by Ahmad Yasser on 20/05/2025.
+//
+
+
+import Foundation
+
+public protocol NetworkClientProtocol {
+    func performRequest<T: Decodable>(_ request: URLRequestConvertible) async throws -> T
+}

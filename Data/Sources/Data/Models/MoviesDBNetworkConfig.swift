d2953bb48468eca7fbce76bb6b8b3207d7bcf557
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2025-05-21 20:55:29 +0300

    Create MoviesDBNetworkConfig.swift

---
+++ Data/Sources/Data/Models/MoviesDBNetworkConfig.swift
@@ -0,0 +1,27 @@
+//
+//  MoviesDBNetworkConfig.swift
+//  Data
+//
+//  Created by Ahmad Yasser on 21/05/2025.
+//
+
+import Foundation
+struct MoviesDBNetworkConfig {
+    static let baseURL: URL = {
+        guard let url = URL(string: "https://api.themoviedb.org/3") else {
+            fatalError("Invalid Base URL")
+        }
+        return url
+    }()
+    
+    // Not secure should be replaced with secured technique
+    static let apiKey = "YOUR_API_KEY_HERE"
+    
+    static var defaultHeaders: [String: String] {
+        return [
+            "Authorization": "Bearer \(apiKey)",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        ]
+    }
+}

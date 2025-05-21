f3770f2c289876eee3fdc7100fe8490dd58889ec
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2025-05-21 11:57:34 +0300

    Create Encodable+Extensions.swift

---
+++ Utils/Sources/Utils/Extensions/Encodable+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  Utils
+//
+//  Created by Ahmad Yasser on 21/05/2025.
+//
+
+import Foundation
+extension Encodable {
+    public func toDictionary() -> [String: Any]? {
+        do {
+            let data = try JSONEncoder().encode(self)
+            let jsonObject = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            return jsonObject as? [String: Any]
+        } catch {
+            print("Failed to convert to dictionary: \(error)")
+            return nil
+        }
+    }
+}

d3b1d1501c61ec398bb2bf82cf79a310141280a0
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2025-05-21 20:57:53 +0300

    Create MovieRepository.swift

---
+++ Data/Sources/Data/Repositories/MovieRepository.swift
@@ -0,0 +1,23 @@
+//
+//  MovieRepository.swift
+//  Data
+//
+//  Created by Ahmad Yasser on 21/05/2025.
+//
+
+import Network
+import Domain
+public final class MovieRepository: MovieRepositoryProtocol {
+    private let networkClient: NetworkClientProtocol
+    
+    public init(networkClient: NetworkClientProtocol = NetworkClient()) {
+        self.networkClient = networkClient
+    }
+    
+    public func getPopularMovies(query: PopularMoviesQueryParameters) async throws -> MovieResponse {
+        try await networkClient.performRequest(MoviesRoute.popular(query: query))
+    }
+    
+    
+}
+

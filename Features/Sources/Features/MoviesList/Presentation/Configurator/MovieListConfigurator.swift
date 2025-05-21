0f79598cdc82a4c4072bcca18e5cc5103ea4ca08
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2025-05-21 23:17:40 +0300

    Create MovieListConfigurator.swift

---
+++ Features/Sources/Features/MoviesList/Presentation/Configurator/MovieListConfigurator.swift
@@ -0,0 +1,23 @@
+//
+//  MovieListConfigurator.swift
+//  Features
+//
+//  Created by Ahmad Yasser on 21/05/2025.
+//
+
+import Domain
+import Data
+import UIKit
+// MARK: - Configurator
+public struct MovieListConfigurator {
+    @MainActor
+    public static func configure() -> UIViewController {
+        // Setup dependencies here
+        let moviesRepo: MovieRepositoryProtocol = MovieRepository()
+        let popularMoviesUseCase = GetPopularMoviesUseCase(repository: moviesRepo)
+        let dependencies = MovieListViewModel.Dependencies(popularMoviesUseCase: popularMoviesUseCase)
+        
+        // Return VC with dependencies injected
+        return MovieListViewController(dependencies: dependencies)
+    }
+}

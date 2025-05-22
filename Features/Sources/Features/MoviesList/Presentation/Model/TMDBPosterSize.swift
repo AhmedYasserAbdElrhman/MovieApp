387ab164318541d93334a1c260c4e14299babeb1
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2025-05-22 21:15:32 +0300

    Create TMDBPosterSize.swift

---
+++ Features/Sources/Features/MoviesList/Presentation/Model/TMDBPosterSize.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBPosterSize.swift
+//  Features
+//
+//  Created by Ahmad Yasser on 22/05/2025.
+//
+
+
+enum TMDBPosterSize: String {
+    case w92 = "w92"       // small thumbnail
+    case w154 = "w154"     // small poster
+    case w185 = "w185"     // medium poster (good for small UIImageViews)
+    case w342 = "w342"     // large poster
+    case w500 = "w500"     // extra large poster
+    case w780 = "w780"     // very large poster
+    case original = "original" // full original size
+}

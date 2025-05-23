a2f2f268089179660cfa276e6daa662cadbdd954
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2025-05-23 16:39:34 +0300

    Create RemoveFromWatchListUseCase.swift

---
+++ Domain/Sources/Domain/UseCases/RemoveFromWatchListUseCase.swift
@@ -0,0 +1,21 @@
+//
+//  RemoveFromWatchListUseCase.swift
+//  Domain
+//
+//  Created by Ahmad Yasser on 23/05/2025.
+//
+
+
+public final class RemoveFromWatchListUseCase: UseCase {
+    public typealias RequestValue = Int
+    public typealias ResponseValue = Void
+    
+    private let localRepository: WatchListLocalRepositoryProtocol
+    
+    public init(localRepository: WatchListLocalRepositoryProtocol) {
+        self.localRepository = localRepository
+    }
+    public func execute(requestValue: Int) async throws -> Void {
+        try await localRepository.removeMovieFromWatchList(requestValue)
+    }
+}

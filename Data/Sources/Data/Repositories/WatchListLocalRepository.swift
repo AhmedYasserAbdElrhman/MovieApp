f31f470cbc35d258e3635c3611a388e81180f002
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2025-05-23 15:42:51 +0300

    Create WatchListLocalRepository.swift

---
+++ Data/Sources/Data/Repositories/WatchListLocalRepository.swift
@@ -0,0 +1,66 @@
+//
+//  WatchListLocalRepository.swift
+//  Data
+//
+//  Created by Ahmad Yasser on 23/05/2025.
+//
+
+import Domain
+import Storage
+import Foundation
+import CoreData
+public class WatchListLocalRepository: WatchListLocalRepositoryProtocol {
+    private let storage: WatchListStack
+    public init(storage: WatchListStack = WatchListStack()) {
+        self.storage = storage
+    }
+    public func addMovieToWatchList(_ movieId: Int) async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            storage.performBackgroundTask { context in
+                let movieCoreData = Storage.Movie(context: context)
+                movieCoreData.id = Int64(movieId)
+                do {
+                    try self.storage.save(context: context)
+                    continuation.resume()
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    public func removeMovieFromWatchList(_ movieId: Int) async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            storage.performBackgroundTask { context in
+                let fetchRequest: NSFetchRequest<Storage.Movie> = Storage.Movie.fetchRequest()
+                fetchRequest.predicate = NSPredicate(format: "id == %d", movieId)
+                fetchRequest.fetchLimit = 1
+                
+                do {
+                    if let movieToDelete = try context.fetch(fetchRequest).first {
+                        context.delete(movieToDelete)
+                        try self.storage.save(context: context)
+                        continuation.resume()
+                    }
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    
+    public func getWatchListMovies() async throws -> [Int] {
+        try await withCheckedThrowingContinuation { continuation in
+            storage.performBackgroundTask { context in
+                let fetchRequest: NSFetchRequest<Storage.Movie> = Storage.Movie.fetchRequest()
+                
+                do {
+                    let movies = try context.fetch(fetchRequest)
+                    let ids = movies.map(\.id).map(Int.init)
+                    continuation.resume(returning: ids)
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+}

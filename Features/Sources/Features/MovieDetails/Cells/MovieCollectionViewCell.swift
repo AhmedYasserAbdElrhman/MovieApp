db11fcd3e12019781f3c943f6d15febd44377311
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2025-05-24 07:20:16 +0300

    Create MovieCollectionViewCell.swift

---
+++ Features/Sources/Features/MovieDetails/Cells/MovieCollectionViewCell.swift
@@ -0,0 +1,62 @@
+//
+//  MovieCollectionViewCell.swift
+//  Features
+//
+//  Created by Ahmad Yasser on 23/05/2025.
+//
+
+
+import UIKit
+
+// MARK: - Movie Collection View Cell
+class MovieCollectionViewCell: UICollectionViewCell {
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 8
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(posterImageView)
+        contentView.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            posterImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            posterImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            posterImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            posterImageView.heightAnchor.constraint(equalToConstant: 150),
+            
+            titleLabel.topAnchor.constraint(equalTo: posterImageView.bottomAnchor, constant: 4),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    func configure(with movie: SimilarMoviePresentationModel) {
+        titleLabel.text = movie.title
+        posterImageView.setImage(with: movie.posterURL())
+    }
+    
+}

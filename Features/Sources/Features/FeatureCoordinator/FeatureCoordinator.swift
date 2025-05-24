d0818613e98e555c245721c04a8941c2ba695e59
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2025-05-24 12:52:23 +0300

    Create FeatureCoordinator.swift

---
+++ Features/Sources/Features/FeatureCoordinator/FeatureCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  FeatureCoordinator.swift
+//  Features
+//
+//  Created by Ahmad Yasser on 24/05/2025.
+//
+
+import UIKit
+public class FeatureCoordinator {
+    weak var navigationController: UINavigationController?
+    public init(navigationController: UINavigationController?) {
+        self.navigationController = navigationController
+    }
+    @MainActor
+    public func start() {
+        let viewController = MovieListConfigurator.configure(navigation: handleMovieNavigation(_:))
+        navigationController?.setViewControllers( [viewController], animated: false)
+    }
+    @MainActor
+    func handleMovieNavigation(_ navigation: MovieListNavigation) {
+        switch navigation {
+        case .movieDetails(let id):
+            let detailViewController = MovieDetailsConfigurator.configure(movieId: id)
+            navigationController?.pushViewController(detailViewController, animated: true)
+        }
+    }
+}

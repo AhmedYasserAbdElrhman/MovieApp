e5e41f3e7b85c04375bac009f21f578838e34fbf
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2025-05-24 12:49:48 +0300

    Create MovieDetailsConfigurator.swift

---
+++ Features/Sources/Features/MovieDetails/Configurator/MovieDetailsConfigurator.swift
@@ -0,0 +1,38 @@
+//
+//  MovieDetailsConfigurator.swift
+//  Features
+//
+//  Created by Ahmad Yasser on 23/05/2025.
+//
+import Domain
+import Data
+import UIKit
+
+// MARK: - Configurator
+struct MovieDetailsConfigurator {
+    @MainActor
+    static func configure(movieId: Int) -> UIViewController {
+        // Setup dependencies here
+        let moviesRepo: MovieRepositoryProtocol = MovieRepository()
+        let getMovieDetailsUseCase = GetMovieDetailsUseCase(repository: moviesRepo)
+        let getSimilarMoviesUseCase = GetSimilarMoviesUseCase(repository: moviesRepo)
+        let getMovieCreditsUseCase = GetMovieCreditsUseCase(repository: moviesRepo)
+        let watchListLocalRepository: WatchListLocalRepositoryProtocol = WatchListLocalRepository()
+        let addToWatchListUseCase = AddToWatchListUseCase(localRepository: watchListLocalRepository)
+        let removeFromWatchListUseCase = RemoveFromWatchListUseCase(localRepository: watchListLocalRepository)
+        let isInWatchListUseCase = IsInWatchListUseCase(localRepository: watchListLocalRepository)
+        let dependencies = MovieDetailsViewModel.Dependencies(
+            movieId: movieId,
+            getMovieDetailsUseCase: getMovieDetailsUseCase,
+            getSimilarMoviesUseCase: getSimilarMoviesUseCase,
+            getMovieCreditsUseCase: getMovieCreditsUseCase,
+            addToWatchListUseCase: addToWatchListUseCase,
+            removeFromWatchListUseCase: removeFromWatchListUseCase,
+            isInWatchListUseCase: isInWatchListUseCase
+        )
+        
+        // Return VC with dependencies injected
+        return MovieDetailsViewController(dependencies: dependencies)
+    }
+}
+

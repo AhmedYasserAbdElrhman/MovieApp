aaf930b1f3c48bb83e88e0a7e5f90d76d7408c52
Ahmed Yasser <52719098+AhmedYasserAbdElrhman@users.noreply.github.com>
2025-05-24 07:19:26 +0300

    Update MovieDetailsViewModel.swift

--- Features/Sources/Features/MovieDetails/ViewModel/MovieDetailsViewModel.swift
+++ Features/Sources/Features/MovieDetails/ViewModel/MovieDetailsViewModel.swift
@@ -48,8 +48,175 @@ class MovieDetailsViewModel: ViewModelType {
     private let topDirectorsSubject = CurrentValueSubject<[PersonPresentationModel], Never>([])
     private let isLoadingSubject = CurrentValueSubject<Bool, Never>(false)
     private let errorSubject = PassthroughSubject<String, Never>()
+    private let isInWatchListSubject = CurrentValueSubject<Bool, Never>(false)
+    
     // MARK: - Initialization
     init(dependencies: Dependencies) {
         self.dependencies = dependencies
+    }
+    // MARK: - Transform
+    func transform(input: Input) -> Output {
+        input.viewDidLoad
+            .sink { [weak self] in
+                guard let self else { return }
+                fetchMovieDetails()
+                checkMovieWatchListExistance()
+            }
+            .store(in: &cancellables)
+        
+        input.watchlistTapped
+            .sink { [weak self] _ in
+                guard let self else { return }
+                if isInWatchListSubject.value {
+                    removeFromWatchlist()
+                } else {
+                    addToWatchlist()
+                }
+            }
+            .store(in: &cancellables)
+        
+        return Output(
+            movieDetails: movieDetailsSubject.eraseToAnyPublisher(),
+            similarMovies: similarMoviesSubject.eraseToAnyPublisher(),
+            topActors: topActorsSubject.eraseToAnyPublisher(),
+            topDirectors: topDirectorsSubject.eraseToAnyPublisher(),
+            isLoading: isLoadingSubject.eraseToAnyPublisher(),
+            error: errorSubject.eraseToAnyPublisher(),
+            isInWatchList: isInWatchListSubject.eraseToAnyPublisher()
+        )
+    }
+    
+    // MARK: - Private Methods
+    private func checkMovieWatchListExistance() {
+        let useCase = dependencies.isInWatchListUseCase
+        let movieId = dependencies.movieId
+        Task {
+            do {
+                let response = try await useCase.execute(requestValue: movieId)
+                await MainActor.run {
+                    isInWatchListSubject.send(response)
+                }
+            } catch {
+                await MainActor.run { self.handleError(errorMessage: error.localizedDescription) }
+            }
+        }
+    }
+    private func fetchMovieDetails() {
+        let useCase = dependencies.getMovieDetailsUseCase
+        let movieId = dependencies.movieId
+        Task {
+            await MainActor.run { isLoadingSubject.send(true) }
+            do {
+                let response = try await useCase.execute(requestValue: movieId)
+                let movieDetails = MovieDetailsPresentationModel(from: response)
+                await MainActor.run {
+                    movieDetailsSubject.send(movieDetails)
+                    isLoadingSubject.send(false)
+                }
+                fetchSimilarMovies(movieId: movieId)
+            } catch {
+                await MainActor.run { self.handleError(errorMessage: error.localizedDescription) }
+            }
+        }
+
+    }
+    
+    private func fetchSimilarMovies(movieId: Int)  {
+        let useCase = dependencies.getSimilarMoviesUseCase
+        Task {
+            await MainActor.run { isLoadingSubject.send(true) }
+            do {
+                let response = try await useCase.execute(requestValue: movieId)
+                let results = response.results
+                let similarMovies = results.map(SimilarMoviePresentationModel.init)
+                await MainActor.run { similarMoviesSubject.send(similarMovies) }
+                // After getting similar movies, fetch credits for each movie
+                fetchCreditsForSimilarMovies(similarMoviesIds: similarMovies.map(\.id))
+            } catch {
+                await MainActor.run { self.handleError(errorMessage: error.localizedDescription) }
+            }
+        }
+    }
+    
+    private func fetchCreditsForSimilarMovies(similarMoviesIds: [Int])  {
+        
+        let useCase = dependencies.getMovieCreditsUseCase
+        Task {
+            await MainActor.run { isLoadingSubject.send(true) }
+            do {
+                var allActors: [PersonPresentationModel] = []
+                var allDirectors: [PersonPresentationModel] = []
+                
+                for id in similarMoviesIds {
+                    let credits = try await useCase.execute(requestValue: id)
+                    let actors = credits.cast.map(PersonPresentationModel.init)
+                    let directors = credits.crew
+                        .filter(\.isDirector)
+                        .map(PersonPresentationModel.init)
+                    allActors.append(contentsOf: actors)
+                    allDirectors.append(contentsOf: directors)
+                }
+                
+                // Group and sort actors by popularity
+                let topActors = Array(Dictionary(grouping: allActors, by: { $0.id })
+                    .values
+                    .compactMap { $0.first }
+                    .sorted(by: { $0.popularity > $1.popularity })
+                    .prefix(5))
+                
+                // Group and sort directors by popularity
+                let topDirectors = Array(Dictionary(grouping: allDirectors, by: { $0.id })
+                    .values
+                    .compactMap { $0.first }
+                    .sorted(by: { $0.popularity > $1.popularity })
+                    .prefix(5))
+                
+                await MainActor.run {
+                    isLoadingSubject.send(false)
+                    topActorsSubject.send(topActors)
+                    topDirectorsSubject.send(topDirectors)
+                }
+            } catch {
+                await MainActor.run { self.handleError(errorMessage: error.localizedDescription) }
+            }
+        }
+    }
+    
+    private func addToWatchlist()  {
+        let useCase = dependencies.addToWatchListUseCase
+        let movieId = dependencies.movieId
+        Task {
+            do {
+                try await useCase.execute(requestValue: movieId)
+                await MainActor.run {
+                    // Add to local cache
+                    isInWatchListSubject.send(true)
+                }
+                
+            } catch {
+                await MainActor.run { self.handleError(errorMessage: error.coreDataFriendlyMessage) }
+            }
+        }
+    }
+    private func removeFromWatchlist()  {
+        let useCase = dependencies.removeFromWatchListUseCase
+        let movieId = dependencies.movieId
+        Task {
+            do {
+                try await useCase.execute(requestValue: movieId)
+                await MainActor.run {
+                    // Add to local cache
+                    isInWatchListSubject.send(false)
+                }
+                
+            } catch {
+                await MainActor.run { self.handleError(errorMessage: error.coreDataFriendlyMessage) }
+            }
+        }
+    }
+    @MainActor
+    private func handleError(errorMessage: String) {
+        self.isLoadingSubject.send(false)
+        self.errorSubject.send(errorMessage)
     }
 }
